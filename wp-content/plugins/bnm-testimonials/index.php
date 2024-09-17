0ac5e2b56743887733c80280327071a705a11b94
Russell Ward <curator@hangar.social>
2024-09-17 13:54:19 -0700

    posts printing out

--- wp-content/plugins/bnm-testimonials/index.php
+++ wp-content/plugins/bnm-testimonials/index.php
@@ -1,3 +1,5 @@
+<?php
+
 /*
 Plugin Name: BNM Testimonials
 Description: Testimonials for Bystanders No More
@@ -5,23 +7,64 @@ Version: 1.0
 Author: Russell Ward
 License: GPL2
 */
+function debug_to_console($data)
+{
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
 
-<?php
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
+
+
+add_shortcode('bnmtestimonials', 'wpbnm_shortcode');
+
+
+function wpbnm_shortcode($atts = [], $content = "")
+{
+    debug_to_console("instance bnm_shortcode");
+    $args = array(
+        'post_type' => 'bnm_testimonial',
+        'posts_per_page' => 10,
+    );
+    $loop = new WP_Query($args);
+    $content = '<div>';
+    while ($loop->have_posts()) {
+        $loop->the_post();
+        $content .= '<div class="entry-content">';
+        $content .= get_the_title();
+        $content .= get_the_content(); 
+        $content .= '</div>';
+    }
+    $content .= '</div>';
+    return $content;
+}
 
 function bnm_custom_testimonial_post()
 {
+    $labels = array(
+        'name' => 'Testimonials',
+        'singular_name' => 'Testimonial',
+        'menu_name' => 'Testimonial',
+        'name_admin_bar' => 'Testimonial',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Testimonial',
+        'new_item' => 'New Testimonial',
+        'edit_item' => 'Edit Testimonial',
+        'view_item' => 'View Testimonial',
+        'all_items' => 'All Testimonials',
+        'search_items' => 'Search Testimonials',
+        'parent_item_colon' => 'Parent Testimonials:',
+        'not_found' => 'No Testimonials found.',
+        'not_found_in_trash' => 'No Testimonials found in Trash.'
+    );
     register_post_type(
         'bnm_testimonial',
         array(
-            'labels' => array(
-                'name' => __('Testimonials', 'textdomain'),
-                'singular_name' => __('Testimonial', 'textdomain'),
-            ),
+            'labels' => $labels,
             'public' => true,
-            'has_archive' => true
-
-            ,
-    ,
+            'has_archive' => true,
             'rewrite' => array('slug' => 'testimonials'),
         )
     );
@@ -29,21 +72,4 @@ function bnm_custom_testimonial_post()
 add_action('init', 'bnm_custom_testimonial_post');
 
 
-add_shortcode('bnmtestimonials', 'bnm_shortcode');
-function bnm_shortcode( $atts = [], $content = null) {
-    $args = array(
-        'post_type'      => 'bnm_testimonial',
-        'posts_per_page' => 10,
-    );
-    $loop = new WP_Query($args);
-    $content = '<div>';
-    while ( $loop->have_posts() ) {
-        $loop->the_post();
-        
-        $content .= '<div class="entry-content">';
-        $content .= the_title();
-        $content .= the_content(); 
-        $content .= '</div>';
-    }
-    return $content;
-}
+
